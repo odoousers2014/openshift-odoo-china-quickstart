e6764f9baf83c120b3568d257f38eed33f4e5b28
buggy <buggy778@qq.com>
2015-05-03 22:59:31 +0800

    1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='Odoo',
       version='8.0',
       description='Odoo on OpenShift',
-      author='Juan Ramón alfaro',
-      author_email='amon.raj@gmail.com',
-      url='https://github.com/amon-ra/openshift-odoo-quickstart',
+      author='buggy',
+      author_email='',
+      url='https://github.com/buggy778/openshift-odoo-china-quickstart',
 )
